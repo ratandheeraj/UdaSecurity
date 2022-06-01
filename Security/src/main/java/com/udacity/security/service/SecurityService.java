92cacc2373253f2a7e1fca48c12a5f61b4797fb5
Ratan Dheeraj <ratandheeraj1@gmail.com>
2022-06-01 10:29:35 +0530

    fixed bug- system should be in pending mode if one sensor is activated

--- Security/src/main/java/com/udacity/security/service/SecurityService.java
+++ Security/src/main/java/com/udacity/security/service/SecurityService.java
@@ -96,9 +96,11 @@ public class SecurityService {
             return; //no problem if the system is disarmed
         }
         switch(securityRepository.getAlarmStatus()) {
-            case NO_ALARM : setAlarmStatus(AlarmStatus.PENDING_ALARM);
-            case PENDING_ALARM : setAlarmStatus(AlarmStatus.ALARM);
+            case NO_ALARM : setAlarmStatus(AlarmStatus.PENDING_ALARM);break;
+            case PENDING_ALARM : setAlarmStatus(AlarmStatus.ALARM);break;
+            default:{}
         }
+
     }
 
     /**
@@ -106,8 +108,9 @@ public class SecurityService {
      */
     private void handleSensorDeactivated() {
         switch(securityRepository.getAlarmStatus()) {
-            case PENDING_ALARM : setAlarmStatus(AlarmStatus.NO_ALARM);
-            case ALARM : setAlarmStatus(AlarmStatus.PENDING_ALARM);
+            case PENDING_ALARM : setAlarmStatus(AlarmStatus.NO_ALARM);break;
+            case ALARM : setAlarmStatus(AlarmStatus.PENDING_ALARM);break;
+            default:{}
         }
     }
 
@@ -126,6 +129,7 @@ public class SecurityService {
         }
         sensor.setActive(active);
         securityRepository.updateSensor(sensor);
+
     }
 
     public void changeSensorActivationStatusWithSensor(Sensor sensor) {
